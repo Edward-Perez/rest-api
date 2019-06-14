814d1bfb98546aa568f4e3f6cc44b41e15505923
Edward Perez <edward.121489@gmail.com>
2019-06-14 12:01:08 +0200

    Add database functionality and rearrange structure

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 'use strict';
 
-// load modules
 const express = require('express');
 const morgan = require('morgan');
-
-// variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
-
-// create the Express app
+const routes = require('./routes');
+const db = require('./models').sequelize;
 const app = express();
+
+// set port
+app.set('port', process.env.PORT || 5000);
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
 
-// TODO setup your api routes here
+// JSON Middleware
+app.use(express.json());
 
-// setup a friendly greeting for the root route
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the REST API project!',
-  });
-});
+// routes 
+app.use('/', routes);
+
+// Database connection
+db.authenticate()
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log('Database Connection Failed', err));
+
+// Database sync
+db.sync()
+  .then(() => {
+    // server listening
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express server is listening on port ${server.address().port}`);
+    });
+  })
+  .catch(err => console.log(err));
+
 
 // send 404 if no other route matched
 app.use((req, res) => {
@@ -29,22 +41,17 @@ app.use((req, res) => {
   });
 });
 
+// variable to enable global error logging
+const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+
 // setup a global error handler
 app.use((err, req, res, next) => {
   if (enableGlobalErrorLogging) {
     console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
   }
-
+  
   res.status(err.status || 500).json({
     message: err.message,
     error: {},
   });
-});
-
-// set our port
-app.set('port', process.env.PORT || 5000);
-
-// start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
 });
