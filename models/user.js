72459ede7fba5e7cc08f7b76674b4067d821b880
Edward Perez <edward.121489@gmail.com>
2019-06-14 11:56:17 +0200

    Update user model for validation and associates

---
+++ models/user.js
@@ -0,0 +1,57 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+
+    firstName: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "First name is required"
+        }
+      }
+    },
+
+    lastName: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Last name is required"
+        }
+      }
+    },
+
+    emailAddress: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Email is required"
+        },
+        isEmail: {
+          msg: "Invalid email"
+        }
+      }
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Password is required"
+        }
+      }
+    },
+    
+  }, {
+    hooks: {
+      beforeCreate: (user) => {
+        user.password = bcrypt.hashSync(user.password, 10);
+      }
+    }
+  });
+  User.associate = function(models) {
+    User.hasMany(models.Course);
+  };
+  return User;
+};
